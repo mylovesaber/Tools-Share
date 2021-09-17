b5475ad7165d35aa57ef2567381b54df78bf2de5
mylovesaber <oyjy95@outlook.com>
2021-09-17 21:42:23 +0800

    增加详细执行日志信息

--- hosts-tool.sh
+++ hosts-tool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Variable initialization
 options=$1
-exec_log=/var/log/hosts-tool/exec-$(date +"%Y-%m-%d").log 2>&1
+exec_log=| tee /var/log/hosts-tool/exec-$(date +"%Y-%m-%d").log 2>&1
 
 _norm=$(tput sgr0)
 _red=$(tput setaf 1)
@@ -48,7 +48,7 @@ function _usage(){
 }
 
 function _placescript(){
-    cd /root
+    _info "开始安装工具..."
     if [[ $extraarg =~ "gitee" ]]; then
         _info "从码云下载脚本"
         wget -qO /usr/bin/hosts-tool https://gitee.com/mylovesaber/auto_update_github_hosts/raw/main/hosts-tool.sh
@@ -56,61 +56,86 @@ function _placescript(){
         _info "从 Github 下载脚本"
         wget -qO /usr/bin/hosts-tool https://raw.githubusercontent.com/mylovesaber/auto_update_github_hosts/master/hosts-tool.sh
     fi
+    _success "已下载到特定路径"
+    _info "修改权限中..."
     chown root: /usr/bin/hosts-tool
     chmod 755 /usr/bin/hosts-tool
+    _success "权限修改完成"
+    _success "工具安装完成"
 }
 
 function _backuphosts(){
-    _info "开始备份hosts.bak 和 hosts.default..."
+    _info "开始备份 hosts.bak 和 hosts.default..."
     # 首次使用此脚本，备份已有 hosts，在清除更新 github hosts 规则的自动环境之前均不会改动此文件
+    _info "检测 hosts.bak 文件是否存在"
     if [[ ! -f /etc/hosts.bak ]]; then
-        _warning "原始hosts.bak备份未找到，开始备份为hosts.bak"
+        _warning "原始 hosts.bak 备份未找到，备份当前 hosts 文件为 hosts.bak"
         cp -af /etc/hosts /etc/hosts.bak
+        _success "备份完毕"
     fi
-    _info "已发现hosts.bak，跳过"
+    _success "已发现hosts.bak，跳过"
     # 将系统自带及用户自定义的 hosts 规则筛选出来并重定向为 hosts.default 文件
     # 用户后续可在特定标记行之后自由修改 hosts 中的信息
     # hosts 文件中特定标记行之后改动的信息会随之后的系统定时任务更新到新的 hosts.default 文件中
+    _info "检测环境安装后实时更新的 hosts.default 文件是否存在"
     cutlinetext="#=========请确保hosts文件中新增的所有内容均在该行之后========="
     if [[ ! -f /etc/hosts.default ]]; then
-        _info "发现hosts.default"
+        _warning "未发现 hosts.default 文件，为原始 hosts 文件文本添加标记并备份为实时更新的 hosts.default..."
         if [[ -n $(grep "${cutlinetext}" /etc/hosts) ]]; then
-            _info "发现切割行"
+            _warning "发现原始 hosts 文件存在残留的标记信息，清理中..."
             sed -i '/'${cutlinetext}'/d' /etc/hosts
+            _success "清理完毕"
         fi
-        _info "开始添加切割行并备份到hosts.default"
+        _info "为原始 hosts 文件添加标记中..."
         sed -i '1i\'${cutlinetext}'' /etc/hosts
+        _success "标记完毕"
+        _info "将原始 hosts 备份为实时更新的 hosts.default..."
         cp -af /etc/hosts /etc/hosts.default
+        _success "备份完毕"
     else
+        _info "未发现hosts.default，将原始 hosts 信息和用户新增内容备份为 hosts.default..."
         cutline=$(grep -n "${cutlinetext}" /etc/hosts | cut -d':' -f1)
-        _info "未发现hosts.default，将hosts中非更新部分备份成该文件"
         sed -n ''${cutline}',$p' /etc/hosts > /etc/hosts.default
+        _success "备份完毕"
     fi
+    _success "所有备份已完成"
 }
 
 function _combine(){
+    _info "开始合并 hosts..."
     if [[ -f /etc/githubhosts.new ]]; then
-        _info "发现githubhosts.new，删掉"
+        _warning "发现githubhosts.new，清理中..."
         rm -rf /etc/githubhosts.new
+        _success "清理完毕"
     fi
-    _info "开始合成新hosts文件"
+    _info "下载最新 Github hosts 信息中..."
     wget -qO /etc/githubhosts.new https://raw.hellogithub.com/hosts
+    _success "下载完成"
+    _info "正在合并并替换成新hosts文件..."
     cat /etc/githubhosts.new > /etc/hosts_combine
     cat /etc/hosts.default >> /etc/hosts_combine
     mv -f /etc/hosts_combine /etc/hosts
+    _success "合并替换完成"
 }
 
 function _setcron(){
-    # 默认每30分钟更新一次hosts，每周自动更新一次工具本身
+    # 默认每30分钟更新一次hosts，每周自动更新一次工具本身，每10天清理一次旧日志
+    _info "清理残留定时任务中..."
     sed -i '/\/usr\/bin\/hosts-tool/d' /etc/crontab
+    _success "清理完成"
+    _info "添加新定时任务中..."
     echo "*/30 * * * * root /usr/bin/bash /usr/bin/hosts-tool run" >> /etc/crontab
-    echo "* */10 * * * root /usr/bin/bash /usr/bin/hosts-tool updatefrom gitee" >> /etc/crontab
+    echo "* */3 * * * root /usr/bin/bash /usr/bin/hosts-tool updatefrom gitee" >> /etc/crontab
     echo "* */10 * * * root /usr/bin/bash /usr/bin/hosts-tool rmlog" >> /etc/crontab
+    _success "新定时任务添加完成"
 }
 
 function _recover(){
+    _warning "开始卸载工具"
     sed -i '/\/usr\/bin\/hosts-tool/d' /etc/crontab
+    _success "定时任务已清除"
     rm -rf /usr/bin/hosts-tool /var/log/hosts-tool/
+    _success "工具和日志记录已清除"
     if [[ ${extraarg} =~ "first_backup" ]]; then
         mv -f /etc/hosts.bak /etc/hosts
         rm -rf /etc/hosts_combine /etc/hosts.default githubhosts.new
@@ -118,6 +143,7 @@ function _recover(){
         mv -f /etc/hosts.default /etc/hosts
         rm -rf /etc/hosts_combine /etc/hosts.bak githubhosts.new
     fi
+    _success "卸载完成，指定 hosts 文件已恢复，拜拜~~"
 }
 
 function _rmlog(){
@@ -136,8 +162,8 @@ if [[ $options == "updatefrom" ]]; then
 		_warning "Github（github）"
 	else
         _loginfo
-        _placescript
-        _setcron
+        _placescript ${exec_log}
+        _setcron ${exec_log}
 	fi
 fi
 
@@ -148,7 +174,6 @@ if [[ $options == "recover" ]]; then
 		_warning "首次备份（first_backup）"
 		_warning "最新备份（uptodate_backup）"
 	else
-        _loginfo
 		_recover
 	fi
 fi
@@ -160,8 +185,8 @@ fi
 
 if [[ $options == "run" ]]; then
     _loginfo
-    _backuphosts
-    _combine
+    _backuphosts ${exec_log}
+    _combine ${exec_log}
 fi
 
 if [[ $options == "rmlog" ]]; then
--- setup.sh
+++ setup.sh
@@ -61,7 +61,7 @@ eval set -- "${ARGS}"
 while true; do
     case "$1" in
     -s | --source)
-        if [[ "$2" =~ "gitee"|"github"|"" ]]; then
+        if [[ "$2" =~ "gitee"|"github" ]]; then
             source="$2"
         else
             _error "参数输入错误，请参照以下使用说明"
@@ -90,50 +90,74 @@ function _placescript(){
     elif [[ $extraarg =~ "github" ]]; then
         _info "从 Github 下载脚本"
         wget -qO /usr/bin/hosts-tool https://raw.githubusercontent.com/mylovesaber/auto_update_github_hosts/master/hosts-tool.sh
-    else
-        _info "未选择下载源，默认从码云下载脚本"
-        wget -qO /usr/bin/hosts-tool https://gitee.com/mylovesaber/auto_update_github_hosts/raw/main/hosts-tool.sh
     fi
     chown root: /usr/bin/hosts-tool
     chmod 755 /usr/bin/hosts-tool
 }
 
 function _backuphosts(){
+    _info "开始备份 hosts.bak 和 hosts.default..."
     # 首次使用此脚本，备份已有 hosts，在清除更新 github hosts 规则的自动环境之前均不会改动此文件
+    _info "检测 hosts.bak 文件是否存在"
     if [[ ! -f /etc/hosts.bak ]]; then
+        _warning "原始 hosts.bak 备份未找到，备份当前 hosts 文件为 hosts.bak"
         cp -af /etc/hosts /etc/hosts.bak
+        _success "备份完毕"
     fi
+    _success "已发现hosts.bak，跳过"
     # 将系统自带及用户自定义的 hosts 规则筛选出来并重定向为 hosts.default 文件
     # 用户后续可在特定标记行之后自由修改 hosts 中的信息
     # hosts 文件中特定标记行之后改动的信息会随之后的系统定时任务更新到新的 hosts.default 文件中
+    _info "检测环境安装后实时更新的 hosts.default 文件是否存在"
     cutlinetext="#=========请确保hosts文件中新增的所有内容均在该行之后========="
     if [[ ! -f /etc/hosts.default ]]; then
+        _warning "未发现 hosts.default 文件，为原始 hosts 文件文本添加标记并备份为实时更新的 hosts.default..."
         if [[ -n $(grep "${cutlinetext}" /etc/hosts) ]]; then
+            _warning "发现原始 hosts 文件存在残留的标记信息，清理中..."
             sed -i '/'${cutlinetext}'/d' /etc/hosts
+            _success "清理完毕"
         fi
+        _info "为原始 hosts 文件添加标记中..."
         sed -i '1i\'${cutlinetext}'' /etc/hosts
+        _success "标记完毕"
+        _info "将原始 hosts 备份为实时更新的 hosts.default..."
         cp -af /etc/hosts /etc/hosts.default
+        _success "备份完毕"
     else
+        _info "未发现hosts.default，将原始 hosts 信息和用户新增内容备份为 hosts.default..."
         cutline=$(grep -n "${cutlinetext}" /etc/hosts | cut -d':' -f1)
         sed -n ''${cutline}',$p' /etc/hosts > /etc/hosts.default
+        _success "备份完毕"
     fi
 }
 
 function _combine(){
+    _info "开始合并 hosts..."
     if [[ -f /etc/githubhosts.new ]]; then
+        _warning "发现githubhosts.new，清理中..."
         rm -rf /etc/githubhosts.new
+        _success "清理完毕"
     fi
+    _info "下载最新 Github hosts 信息中..."
     wget -qO /etc/githubhosts.new https://raw.hellogithub.com/hosts
+    _success "下载完成"
+    _info "正在合并并替换成新hosts文件..."
     cat /etc/githubhosts.new > /etc/hosts_combine
     cat /etc/hosts.default >> /etc/hosts_combine
     mv -f /etc/hosts_combine /etc/hosts
+    _success "合并替换完成"
 }
 
 function _setcron(){
+    # 默认每30分钟更新一次hosts，每周自动更新一次工具本身，每10天清理一次旧日志
+    _info "清理残留定时任务中..."
     sed -i '/\/usr\/bin\/hosts-tool/d' /etc/crontab
+    _success "清理完成"
+    _info "添加新定时任务中..."
     echo "*/30 * * * * root /usr/bin/bash /usr/bin/hosts-tool run" >> /etc/crontab
-    echo "* */7 * * * root /usr/bin/bash /usr/bin/hosts-tool updatefrom gitee" >> /etc/crontab
+    echo "* */3 * * * root /usr/bin/bash /usr/bin/hosts-tool updatefrom gitee" >> /etc/crontab
     echo "* */10 * * * root /usr/bin/bash /usr/bin/hosts-tool rmlog" >> /etc/crontab
+    _success "新定时任务添加完成"
 }
 
 function _showinfo(){
@@ -153,3 +177,4 @@ if [[ ! -d /var/log/hosts-tool ]]; then
     mkdir -p /var/log/hosts-tool
 fi
 _main | tee /var/log/hosts-tool/install.log
+_showinfo
