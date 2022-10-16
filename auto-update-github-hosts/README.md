668c85a035292b2ab714db57bb27e2681fbeea41
mylovesaber <oyjy95@outlook.com>
2022-10-17 01:25:10 +0800

    更新 auto-update-github-hosts 代码准备测试

--- auto-update-github-hosts/README.md
+++ auto-update-github-hosts/README.md
@@ -17,13 +17,13 @@
 - Synology(群晖)
 - MacOS (Menterey 测试通过，理论上文件系统是 APFS 的新一代系统应该都支持)
 
-注意：MacOS 每次系统版本更新后都得重新跑一遍安装脚本或者命令 `hosts-tool updatefrom [gitlab/github]` ，否则定时功能会被苹果清空
+注意：MacOS 每次系统版本更新后都得重新跑一遍安装脚本或者命令 `hosts-tool update` 或 `hosts-tool updatefrom [gitlab/github]` ，否则定时功能会被苹果清空
 
 其他系统或版本有需要适配的请发 issue 并提供所用系统版本和对应可用的 DNS 刷新功能的方法，如果有需要安装软件包的话请提供下包名
 
 ~unRAID 系统定时功能做了定制，所以单独适配了，手动控制方式上没有区别，工具会自动识别。~
 
-unRAID 系统适配好像没完全生效。。。等以后单独测试吧。。。
+unRAID 系统适配好像没完全生效。。。等以后用这系统再单独测试吧。。。
 
 # 工具说明、功能介绍、安装步骤
 
@@ -31,13 +31,17 @@ unRAID 系统适配好像没完全生效。。。等以后单独测试吧。。�
 2022年5月18日，码云（Gitee）启动了代码审查机制，但凡不合规矩或者存在个别字眼比较敏感的都可能被删除或阻止其他用户访问，
 且默认阻断匿名访问，鉴于本工具骨骼清奇，码云平台已彻底屏蔽此项目，所以该平台我已彻底放弃。
 经过测试，中国大陆大部分地区没有特殊手段无法打开 GitHub 网站，但基本都能打开 GitLab，于是我设置成中国大陆用户默认从 GitLab 获取工具源码。
-不要有非 GitHub 不用的思想，我代码都是首先同步到 GitLab，然后再由 GitLab 自动同步到 GitHub 上。
+不要有非 GitHub 不用的思想，我代码都是首先 push 到 GitLab，然后再由 GitLab 自动同步到 GitHub 上。
 
-GitHub： https://github.com/mylovesaber/auto_update_github_hosts
+GitHub： https://github.com/mylovesaber/Tools-Share/tree/main/auto-update-github-hosts
 
-GitLab：  https://gitlab.com/mylovesaber/auto_update_github_hosts
+GitLab：  https://gitlab.com/mylovesaber/tools-share/-/tree/main/auto-update-github-hosts
 
-项目内含两个脚本，分别为安装脚本和日用脚本。 **首次请运行安装脚本，如果未来日用脚本失效了，请重新运行安装脚本即可**
+项目内含两个脚本，分别为安装脚本和日用脚本。
+
+**首次请运行安装脚本，如果未来日用脚本失效了，请重新运行安装脚本即可**
+
+**如果不想用此工具但因日用脚本失效导致系统中存在残留，请先运行安装脚本，然后运行日用脚本完成清理操作**
 
 两个脚本均存在当检测到 GitHub 源无法正常连通时就自动切换到 GitLab 源进行更新的逻辑判断，并且一旦 GitHub 源更新失败的时候，自动更新工具在切换到 GitLab 源并更新本身之后，将把未来自动更新工具的源头替换为 GitLab 源，如果有人执意要用 GitHub 更新的话，请手动运行一次日用脚本中的更新工具本身并指定 GitHub 为更新源(说真的，没必要，因为两个源是同步的，使用哪个都一样...)
 
@@ -71,11 +75,10 @@ setup.sh  选项  参数
 
 ```bash
 # GitLab 托管的安装脚本 + 指定 GitLab 为日用脚本安装源
-bash <(curl -Ls https://gitlab.com/api/v4/projects/36462411/repository/files/setup.sh/raw?ref=main) -s gitlab
+bash <(curl -Ls https://gitlab.com/api/v4/projects/37571126/repository/files/auto%2Dupdate%2Dgithub%2Dhosts%2Fsetup%2Esh/raw?ref=dev) -s gitlab
 
 # GitHub 托管的安装脚本 + 指定 GitLab 为日用脚本安装源
-bash <(curl -Ls https://github.com/mylovesaber/auto_update_github_hosts/raw/main/setup.sh) -s gitlab
-
+bash <(curl -Ls https://raw.githubusercontent.com/mylovesaber/Tools-Share/dev/auto_update_github_hosts/setup.sh) -s gitlab
 ```
 
 ## 日用脚本
@@ -85,6 +88,7 @@ bash <(curl -Ls https://github.com/mylovesaber/auto_update_github_hosts/raw/main
 - 更新工具本身
 - 更新 hosts 并刷新 dns
 - 完全卸载
+- 移除过期日志
 
 自带的帮助信息如下：
 
@@ -95,31 +99,32 @@ GitHub hosts 自动部署和更新工具
 hosts-tool  选项1  (选项2)
 
 选项:
-
 run                        立即更新hosts
-updatefrom gitlab|github    需指定下载源才能升级该工具
-                           可选选项为 gitlab 或 github，默认是 GitLab
 
-recover                    该选项将将此工具所有功能从系统中移除
-                           可选选项为 first_backup 或 uptodate_backup
+update                     默认从 GitLab 升级该工具
+
+updatefrom gitlab|github   需指定下载源才能升级该工具
+                           可选选项为 gitlab 或 github
+                           
+remove                     该选项将将此工具和生成的各种文件从系统中移除并还原 host 文件内容
 
 help                       显示帮助信息并退出
+
 ```
 
 日用命令：
 
 ```bash
-# 即时更新 hosts（同时会立即备份 /etc/hosts 中新增的各种dns解析规则）
+# 即时更新 hosts（同时会立即备份 /etc/hosts 中用户自行增加的各种dns解析规则）
 hosts-tool run
 
-# 即时更新此工具本身（以下有两种源，择一运行即可）
+# 默认从 GitLab 立即更新此工具本身
+hosts-tool update
+
+# 立即根据指定的更新源更新此工具本身（以下有两种源，择一运行即可），推荐上面那个更新命令
 hosts-tool updatefrom gitlab
 hosts-tool updatefrom github
 
-# 完全卸载工具（恢复第一次安装工具时备份的 hosts 文件，会丢弃日后新增的其他各种dns解析规则）
-hosts-tool recover first_backup
-
-# 完全卸载工具（恢复最后一次备份的hosts文件，推荐运行此卸载命令前先执行 hosts-tool run）
-hosts-tool recover uptodate_backup
-
+# 完全卸载工具
+hosts-tool remove
 ```
--- auto-update-github-hosts/setup.sh
+++ auto-update-github-hosts/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Variable initialization
-source=gitee
+downloadSource="gitlab"
 ExtraArgs=$1
-SYSTEM_TYPE=
+cutLineText="#=========请确保hosts文件中新增的所有内容均在该行之后========="
+systemType=
 
 # if ! which tput > /dev/null 2>&1; then
 _norm="\033[39m"
@@ -35,51 +36,54 @@ function _error() {
 	printf "${_red}✗ %s${_norm}\n" "$@"
 }
 
-function _checkroot() {
+function CheckRoot() {
 	if [ $EUID != 0 ] || [[ $(grep "^$(whoami)" /etc/passwd | cut -d':' -f3) != 0 ]]; then
         _error "没有 root 权限，请运行 \"sudo su -\" 命令并重新运行该脚本"
 		exit 1
 	fi
 }
-_checkroot
+CheckRoot
 
-function _checksys(){
+function CheckSys(){
     _info "正在检查系统兼容性..."
+    local systemName
     if [ -f /usr/bin/sw_vers ]; then
-        SYSTEM_TYPE="MacOS"
+        systemType="MacOS"
     elif [ -f /usr/bin/lsb_release ]; then
-        system_name=$(lsb_release -i 2>/dev/null)
-        if [[ ${system_name} =~ "Debian" ]]; then
-            SYSTEM_TYPE="Debian"
-        elif [[ ${system_name} =~ "Ubuntu" ]]; then
-            SYSTEM_TYPE="Ubuntu"
+        systemName=$(lsb_release -i 2>/dev/null)
+        if [[ ${systemName} =~ "Debian" ]]; then
+            systemType="Debian"
+        elif [[ ${systemName} =~ "Ubuntu" ]]; then
+            systemType="Ubuntu"
         fi
     elif [ -f /etc/redhat-release ]; then
-        system_name=$(cat /etc/redhat-release 2>/dev/null)
-        if [[ ${system_name} =~ "CentOS" ]]; then
-            SYSTEM_TYPE="CentOS"
-        elif [[ ${system_name} =~ "Red" ]]; then
-            SYSTEM_TYPE="RedHat"
+        systemName=$(cat /etc/redhat-release 2>/dev/null)
+        if [[ ${systemName} =~ "CentOS" ]]; then
+            systemType="CentOS"
+        elif [[ ${systemName} =~ "Red" ]]; then
+            systemType="RedHat"
         fi
     elif which synoservicectl > /dev/null 2>&1; then
-        SYSTEM_TYPE="Synology"
+        systemType="Synology"
     # elif which opkg > /dev/null 2>&1; then
-    #     SYSTEM_TYPE="ROUTER"
+    #     systemType="ROUTER"
     # elif [[ $(find / -name *unRAID* 2>/dev/null |xargs) =~ "unRAID" ]]; then
-    #     SYSTEM_TYPE="unRAID"
+    #     systemType="unRAID"
     else
         _error "暂未适配该系统，退出..."
         exit 1
     fi
-    _success "当前系统为： ${SYSTEM_TYPE} 此脚本支持该系统！"
+    _success "当前系统为： ${systemType} 此脚本支持该系统！"
 }
 
-function _usage(){
+function Usage(){
 	# print help info
-	echo -e "\nGitHub hosts 自动部署和更新工具"
-	echo -e "\n命令格式: \nsetup.sh  选项  参数"
+	echo ""
+	echo "GitHub hosts 自动部署和更新工具"
+	echo -e "\n命令格式: \n"
+	echo "setup.sh  选项  参数"
 	echo -e "\n选项:\n"
-	echo "-s 或 --source        指定下载源，可选参数为 gitlab 或 github，若不使用该选项则默认从 GitLab 下载"
+	echo "-s 或 --source        指定下载源，可选参数为 \"gitlab\" 或 \"github\"，若不使用该选项则默认从 GitLab 下载"
 	echo "-h 或 --help          显示帮助信息并退出"
 }
 
@@ -88,7 +92,7 @@ function _usage(){
 if ! ARGS=$(getopt -a -o hs: -l help,source: -- "$@")
 then
     _error "选项输入有误，请参照以下使用说明"
-    _usage
+    Usage
     exit 1
 fi
 eval set -- "${ARGS}"
@@ -96,16 +100,16 @@ while true; do
     case "$1" in
     -s | --source)
         if [[ "$2" =~ "gitlab"|"github" ]]; then
-            source="$2"
+            downloadSource="$2"
         else
             _error "参数输入错误，请参照以下使用说明"
-            _usage
+            Usage
             exit 1
         fi
         shift
         ;;
     -h | --help)
-        _usage
+        Usage
         exit 1
         ;;
     --)
@@ -116,48 +120,59 @@ while true; do
     shift
 done
 
-function _placescript(){
+function PlaceScript(){
     _info "开始安装工具..."
     if ! which timeout > /dev/null 2>&1; then
-        # if [[ "${SYSTEM_TYPE}" == "ROUTER" ]]; then
+        # if [[ "${systemType}" == "ROUTER" ]]; then
         #     _info "开始安装 timeout"
         #     opkg update > /dev/null 2>&1
         #     opkg install coreutils-timeout
         #     _success "timeout 安装完成"
         # else
-        _warning "未发现 timeout 命令，将临时下载 timeout 命令程序"
-        curl -Ls https://gitlab.com/api/v4/projects/36462411/repository/files/timeout/raw?ref=main
-        chmod +x /tmp/timeout
-        export PATH="/tmp:$PATH"
-        _success "timeout 命令程序已下载并应用成功"
-        # fi
-    fi
-    count=1
-    while true;do
-        if [[ $source == "gitlab" ]]; then
-            _info "从 GitLab 下载脚本"
-            timeout 20s curl -Ls https://gitlab.com/api/v4/projects/36462411/repository/files/hosts-tool.sh/raw?ref=main -o /tmp/hosts-tool
-        elif [[ $source == "github" ]]; then
-            _info "从 GitHub 下载脚本"
-            _info "开始检测 GitHub 连通性..."
-            if ! timeout 5s ping -c2 -W1 github.com > /dev/null 2>&1; then
-                _error "本机所在网络无法连接 GitHub，将切换到 GitLab 同步源进行更新..."
-                source="gitlab"
-                continue
-            else
-                timeout 5s wget -qO /tmp/hosts-tool https://raw.githubusercontent.com/mylovesaber/auto_update_github_hosts/master/hosts-tool.sh
-            fi
+        if [ "${systemType}" = "MacOS" ]; then
+            _warning "MacOS 未发现 timeout 命令，将临时下载 timeout 程序"
+            curl -Ls https://gitlab.com/api/v4/projects/37571126/repository/files/auto%2Dupdate%2Dgithub%2Dhosts%2Ftimeout/raw?ref=dev -o /usr/local/bin/hosts-tool
+            _info "修改权限中..."
+            chown root:admin /usr/local/bin/hosts-tool
+            chmod 755 /usr/local/bin/hosts-tool
+            _success "timeout 程序已下载并应用成功"
+        else
+            _error "暂未适配，请联系作者"
         fi
+    fi
+    local COUNT=1
+    while true;do
+        case "${downloadSource}" in
+            "gitlab")
+                _info "从 GitLab 下载脚本"
+                timeout 20s curl -Ls https://gitlab.com/api/v4/projects/37571126/repository/files/auto%2Dupdate%2Dgithub%2Dhosts%2Fhosts%2Dtool%2Esh/raw?ref=dev -o /tmp/hosts-tool
+                ;;
+            "github")
+                _info "从 GitHub 下载脚本"
+                _info "开始检测 GitHub 连通性..."
+                if ! timeout 5s ping -c2 -W1 github.com > /dev/null 2>&1; then
+                    _error "本机所在网络无法连接 GitHub，将切换到 GitLab 同步源进行更新..."
+                    downloadSource="gitlab"
+                    continue
+                else
+                    timeout 5s wget -qO /tmp/hosts-tool https://raw.githubusercontent.com/mylovesaber/auto_update_github_hosts/master/hosts-tool.sh
+                fi
+                ;;
+            *)
+                _error "参数输入错误，请参照以下使用说明"
+                Usage
+                exit 1
+        esac
         if [ -f /tmp/hosts-tool ] && [ -n "$(cat /tmp/hosts-tool)" ];then
             _success "已下载，开始转移到系统程序路径"
             break
         else
             sleep 1
-            count=$(( count + 1 ))
-            _error "下载文件不存在，开始尝试第 ${count} 次下载..."
+            COUNT=$(( COUNT + 1 ))
+            _error "下载的 hosts-tool 不存在，开始尝试第 ${COUNT} 次下载..."
         fi
-        if [ "${count}" -gt 5 ]; then
-            _error "工具下载失败，请择日再运行此脚本，退出中..."
+        if [ "${COUNT}" -gt 5 ]; then
+            _error "hosts-tool 下载失败，请择日再运行此脚本，退出中..."
             exit 1
         fi
     done
@@ -165,12 +180,12 @@ function _placescript(){
         _error "安装出现错误，退出中..."
         exit 1
     fi
-    if [[ "${SYSTEM_TYPE}" == "MacOS" ]]; then
+    if [[ "${systemType}" == "MacOS" ]]; then
         mv /tmp/hosts-tool /usr/local/bin/hosts-tool
         _info "修改权限中..."
         chown root:admin /usr/local/bin/hosts-tool
         chmod 755 /usr/local/bin/hosts-tool
-    # elif [[ "${SYSTEM_TYPE}" == "ROUTER" ]]; then
+    # elif [[ "${systemType}" == "ROUTER" ]]; then
     #     mv /tmp/hosts-tool /opt/bin/hosts-tool
     #     _info "修改权限中..."
     #     chmod 755 /opt/bin/hosts-tool
@@ -184,76 +199,74 @@ function _placescript(){
     _success "工具安装完成"
 }
 
-function _backuphosts(){
-    _info "开始备份 hosts.bak 和 hosts.default..."
-    # 首次使用此脚本，备份已有 hosts，在清除更新 github hosts 规则的自动环境之前均不会改动此文件
-    _info "检测 hosts.bak 文件是否存在"
-    if [[ ! -f /etc/hosts.bak ]]; then
-        _warning "原始 hosts.bak 备份未找到，备份当前 hosts 文件为 hosts.bak"
-        cp -af /etc/hosts /etc/hosts.bak
-        _success "备份完毕"
-    fi
-    _success "已发现hosts.bak，跳过"
-    # 将系统自带及用户自定义的 hosts 规则筛选出来并重定向为 hosts.default 文件
+function BackupHosts(){
+    # 永远根据当前 hosts 内容，将删除 github host 有关信息后的内容全部备份为 hosts.default 文件
     # 用户后续可在特定标记行之后自由修改 hosts 中的信息
     # hosts 文件中特定标记行之后改动的信息会随之后的系统定时任务更新到新的 hosts.default 文件中
-    _info "检测环境安装后实时更新的 hosts.default 文件是否存在"
-    cutlinetext="#=========请确保hosts文件中新增的所有内容均在该行之后========="
-    if [[ ! -f /etc/hosts.default ]]; then
-        _warning "未发现 hosts.default 文件，为原始 hosts 文件文本添加标记并备份为实时更新的 hosts.default..."
-        if [[ -n $(grep "${cutlinetext}" /etc/hosts) ]]; then
-            _warning "发现原始 hosts 文件存在残留的标记信息，清理中..."
-            sed -i '/'${cutlinetext}'/d' /etc/hosts
-            _success "清理完毕"
-        fi
-        _info "为原始 hosts 文件添加标记中..."
-        sed -i '1i\'${cutlinetext}'' /etc/hosts
-        _success "标记完毕"
-        _info "将原始 hosts 备份为实时更新的 hosts.default..."
-        cp -af /etc/hosts /etc/hosts.default
-        _success "备份完毕"
-    else
-        _info "未发现hosts.default，将原始 hosts 信息和用户新增内容备份为 hosts.default..."
-        cutline=$(grep -n "${cutlinetext}" /etc/hosts | cut -d':' -f1)
-        sed -n ''${cutline}',$p' /etc/hosts > /etc/hosts.default
-        _success "备份完毕"
+    _info "为原始 hosts 文件文本添加标记并备份为实时更新的 hosts.default..."
+    if grep -q "${cutLineText}" /etc/hosts; then
+        _warning "发现原始 hosts 文件存在残留的标记信息，开始删除标记信息及以上的所有陈旧信息..."
+        local cutLineTextNum
+        cutLineTextNum=$(awk /${cutLineText}/'{print NR}' /etc/hosts)
+        sed -i "1,${cutLineTextNum}d" /etc/hosts
+        _success "陈旧信息删除完毕"
     fi
-    _success "所有备份已完成"
+    _info "为 hosts 文件添加标记中..."
+    sed -i "1i${cutLineText}" /etc/hosts
+    _success "标记完毕"
+    _info "将原始 hosts 备份为实时更新的 hosts.default..."
+    cp -af /etc/hosts /etc/hosts.default
+    _success "备份完毕"
 }
 
-function _combine(){
-    _info "开始合并 hosts..."
+function Combine(){
     if [[ -f /etc/githubhosts.new ]]; then
-        _warning "发现githubhosts.new，清理中..."
+        _warning "发现 githubhosts.new，清理中..."
         rm -rf /etc/githubhosts.new
         _success "清理完毕"
     fi
     _info "下载最新 GitHub hosts 信息中..."
-    wget -qO /etc/githubhosts.new https://raw.hellogithub.com/hosts
-    _success "下载完成"
-    _info "正在合并并替换成新hosts文件..."
-    cat /etc/githubhosts.new > /etc/hosts_combine
-    cat /etc/hosts.default >> /etc/hosts_combine
-    mv -f /etc/hosts_combine /etc/hosts
-    sed -i '/^</d' /etc/hosts
-    _success "合并替换完成"
+    local newIP
+    local COUNT=0
+    while true; do
+        newIP=$(curl -Ls https://raw.hellogithub.com/hosts|sed '/^</d')
+        if [ -n "${newIP}" ]; then
+            _success "下载完成"
+            _info "正在合并并替换成新hosts文件..."
+            echo "${newIP}" > /etc/hosts_combine
+            cat /etc/hosts.default >> /etc/hosts_combine
+            mv -f /etc/hosts_combine /etc/hosts
+            _success "合并替换完成"
+            break
+        elif [ ${COUNT} -le 5 ]; then
+            COUNT=$((COUNT + 1))
+            _error "获取失败，准备尝试第 ${COUNT} 次获取"
+            continue
+        else
+            _error "获取失败，退出中"
+            exit 1
+        fi
+    done
 }
 
-function _refresh_dns(){
+function RefreshDNS(){
     _info "正在刷新 DNS 缓存..."
     # 路由器中的 bash 不知道啥原因不识别 =~ 会报错，以下其他系统都正常
-    # if [[ "${SYSTEM_TYPE}" == "ROUTER" ]]; then
+    # if [[ "${systemType}" == "ROUTER" ]]; then
     #     if ! which restart_dns > /dev/null 2>&1; then
     #         _error "暂未发现该系统中的刷新 dns 功能，请自行搜索该系统的刷新 dns 方法并给脚本作者发 issue"
     #         exit 1
     #     fi
     #     restart_dns
-    # elif [[ "${SYSTEM_TYPE}" =~ "Ubuntu"|"Debian"|"RedHat" ]]; then
-    if [[ "${SYSTEM_TYPE}" =~ "Ubuntu"|"Debian"|"RedHat" ]]; then
+    # elif [[ "${systemType}" =~ "Ubuntu"|"Debian"|"RedHat" ]]; then
+    if [[ "${systemType}" =~ "Ubuntu"|"Debian"|"RedHat" ]]; then
+        if [ "$(systemctl is-active NetworkManager)" = "active" ] && [ "$(systemctl is-enabled systemd-resolved.service)" = "disabled" ]; then
+            systemctl enable systemd-resolved --now 1>& /dev/null
+        fi
         systemd-resolve --flush-caches
-    elif [[ "${SYSTEM_TYPE}" == "MacOS" ]]; then
+    elif [[ "${systemType}" == "MacOS" ]]; then
         killall -HUP mDNSResponder
-    elif [[ "${SYSTEM_TYPE}" == "CentOS" ]]; then
+    elif [[ "${systemType}" == "CentOS" ]]; then
         if ! which nscd > /dev/null 2>&1; then
             if ! which dnf > /dev/null 2>&1; then
                 yum install -y nscd
@@ -262,49 +275,50 @@ function _refresh_dns(){
             fi
         fi
         systemctl restart nscd
-    elif [[ "${SYSTEM_TYPE}" == "Synology" ]]; then
+    elif [[ "${systemType}" == "Synology" ]]; then
         /var/packages/DNSServer/target/script/flushcache.sh
     fi
     _success "DNS 缓存刷新完成"
 }
 
-function _setcron(){
+function SetCron(){
     # 默认每1小时更新一次hosts，每3天自动更新一次工具本身，每10天清理一次旧日志
-    if [[ "${SYSTEM_TYPE}" == "MacOS" ]]; then
+    if [[ "${systemType}" == "MacOS" ]]; then
         _info "清理残留定时任务中..."
         crontab -l | grep -v "hosts-tool" | crontab -
         _success "清理完成"
         _info "添加新定时任务中..."
         {
             echo "* */1 * * * /usr/local/bin/hosts-tool run"
-            echo "* * */3 * * /usr/local/bin/hosts-tool updatefrom $source"
+            echo "* * */3 * * /usr/local/bin/hosts-tool updatefrom $downloadSource"
             echo "* * */10 * * /usr/local/bin/hosts-tool rmlog"
         } >> /tmp/cronfile
         crontab /tmp/cronfile
         rm -rf /tmp/cronfile
         _success "新定时任务添加完成"
-    # elif [[ "${SYSTEM_TYPE}" == "ROUTER" ]]; then
+    # elif [[ "${systemType}" == "ROUTER" ]]; then
     #     _info "清理残留定时任务中..."
     #     crontab -l | grep -v "hosts-tool" | crontab -
     #     _success "清理完成"
     #     _info "添加新定时任务中..."
     #     {
     #         echo "* */1 * * * /opt/bin/hosts-tool run"
-    #         echo "* * */3 * * /opt/bin/hosts-tool updatefrom $source"
+    #         echo "* * */3 * * /opt/bin/hosts-tool updatefrom $downloadSource"
     #         echo "* * */10 * * /opt/bin/hosts-tool rmlog"
     #     } >> /tmp/cronfile
     #     crontab /tmp/cronfile
     #     rm -rf /tmp/cronfile
     #     _success "新定时任务添加完成"
-    elif [[ ${SYSTEM_TYPE} =~ "unRAID" ]]; then
+    elif [[ ${systemType} =~ "unRAID" ]]; then
         _info "清理残留定时任务中..."
-        hostpath=/boot/config/plugins/dynamix/github-hosts.cron
-        rm -rf $hostpath
+        local hostPath
+        hostPath=/boot/config/plugins/dynamix/github-hosts.cron
+        rm -rf $hostPath
         _success "清理完成"
         _info "添加新定时任务中..."
-        cat >> $hostpath <<EOF
+        cat >> $hostPath <<EOF
 * */1 * * * root /usr/bin/bash /usr/bin/hosts-tool run
-* * */3 * * root /usr/bin/bash /usr/bin/hosts-tool updatefrom $source
+* * */3 * * root /usr/bin/bash /usr/bin/hosts-tool updatefrom $downloadSource
 * * */10 * * root /usr/bin/bash /usr/bin/hosts-tool rmlog
 EOF
         /usr/local/sbin/update_cron
@@ -316,35 +330,35 @@ EOF
         _info "添加新定时任务中..."
         {
             echo "* */1 * * * root /usr/bin/bash /usr/bin/hosts-tool run"
-            echo "* * */3 * * root /usr/bin/bash /usr/bin/hosts-tool updatefrom $source"
+            echo "* * */3 * * root /usr/bin/bash /usr/bin/hosts-tool updatefrom $downloadSource"
             echo "* * */10 * * root /usr/bin/bash /usr/bin/hosts-tool rmlog"
         } >> /etc/crontab
         _success "新定时任务添加完成"
     fi
 }
 
-function _showinfo(){
+function ShowInfo(){
     _success "GitHub hosts 自动部署和更新工具已安装完成并开启自动更新"
     _success "命令行输入：hosts-tool 或 hosts-tool help 即可查看具体控制选项"
 }
 
-function _main(){
-    _checksys
-    _placescript
-    _backuphosts
-    _combine
-    _refresh_dns
-    _setcron
-    _showinfo
+function Main(){
+    CheckSys
+    PlaceScript
+    BackupHosts
+    Combine
+    RefreshDNS
+    SetCron
+    ShowInfo
 }
 
 if [[ -z $ExtraArgs ]];then
     _error "未输入选项，请参照以下使用说明运行该程序"
-    _usage
+    Usage
     exit 1
 fi
 
 if [[ ! -d /var/log/hosts-tool ]]; then
     mkdir -p /var/log/hosts-tool
 fi
-_main | tee /var/log/hosts-tool/install.log
+Main | tee /var/log/hosts-tool/install.log
