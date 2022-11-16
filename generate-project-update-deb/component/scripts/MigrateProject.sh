cc57f47ddb0f79301ba1000d63948aa9d7a62d0a
mylovesaber <oyjy95@outlook.com>
2022-11-16 08:51:24 +0800

    generate-project-update-deb bug修复

--- generate-project-update-deb/component/scripts/MigrateProject.sh
+++ generate-project-update-deb/component/scripts/MigrateProject.sh
@@ -13,7 +13,7 @@ withoutMigrateFolderName="WITHOUT_MIGRATE_FOLDER_NAME"
 packageSource="PACKAGE_SOURCE"
 
 cp -a /tmp/$packageSource/$tomcatNewName $packageDeployPath
-mapfile -t needMigrateFolderNameList < <(find $packageDeployPath -maxdepth 1 -type d|grep -v "$defaultWebappsFolderNameLine\|$withoutMigrateFolderName")
+mapfile -t needMigrateFolderNameList < <(find $packageDeployPath/tomcat-$tomcatLatestRunningVersion-$tomcatPreviousPort -maxdepth 1 -type d|grep -v "$defaultWebappsFolderNameLine\|$withoutMigrateFolderName")
 for i in "${needMigrateFolderNameList[@]}";do
     cp -a "$i" "$packageDeployPath/$tomcatNewName/webapps"
 done
